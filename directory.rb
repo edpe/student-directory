7385812a897af1c39bb02c7ea7693a5ed5bd2607
Ed Perkins <ed@edperkins.co.uk>
2017-12-12 10:01:11 +0000

    Add methods

--- directory.rb
+++ directory.rb
@@ -12,11 +12,21 @@ students = [
  "Norman Bates"
 ]
 
-puts "The Students of Villains Academy"
-puts "----------"
-students.each do |student|
-  puts students
+def print_header
+  puts "The Students of Villains Academy"
+  puts "----------"
 end
 
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+  puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
