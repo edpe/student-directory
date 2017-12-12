4e48b38d4d34f6796a7ed450373b5881abb70ee1
Ed Perkins <ed@edperkins.co.uk>
2017-12-12 12:03:52 +0000

    add first letter of name selection

--- directory.rb
+++ directory.rb
@@ -17,9 +17,13 @@ def print_header
 end
 
 def print(students)
+  puts "Enter the first letter of the names of students to display"
+  first_letter = gets.chomp
   students.each_with_index do |student, index|
+    if student[:name][0] == first_letter
   puts " #{(index + 1)} #{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
 end
 
 def print_footer(names)
