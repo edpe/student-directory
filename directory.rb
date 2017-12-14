0013c7e56baa4d3e560aad3a0680b4eae93dbddc
Ed Perkins <ed@edperkins.co.uk>
2017-12-14 11:16:09 +0000

    refactor into methods

--- directory.rb
+++ directory.rb
@@ -1,109 +1,65 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
-  students = []
-  months = ["january",
-            "february",
-            "march",
-            "april",
-            "may",
-            "june",
-            "july",
-            "august",
-            "september",
-            "october",
-            "november",
-            "december"]
-
-  while true do
-    puts "Please enter student name:"
-    name = gets.slice(0..-2)
-    if name.empty?
-    name = "no name given"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
-while true do
-    puts "Please enter cohort:"
-    cohort = gets.slice(0..-2).downcase
-
-
-      if cohort.empty?
-      cohort = "no recorded"
-      break
-
-      elsif !months.include?(cohort)
-      puts "That doesn't seem to be a month I recognise, please try again or press enter twice to leave blank"
-
-    else
-      break
-    end
-
-end
-
-if name == "no name given" && cohort == "no recorded"
-  puts "no students added"
-else
-  students << {name: name, cohort: cohort.to_sym}
-end
-
-  puts "Now we have #{students.count} students"
-
-  puts "Do you wish to add another student y/n?"
-  choice = gets.slice(0..-2)
-    if choice != "y"
-    break
-  end
-  end
-students
-end
-
-def print_header
-  puts "The Students of Villains Academy".center(80)
-  puts "----------".center(80)
-end
-
-def print(students)
-  #generates array of all cohorts
-  cohort_array = students.map {|x| x.fetch(:cohort)}.uniq
-  cohort_array.each do |cohort|
-    puts students.select { |students| puts "#{students[:name]}, #{students[:cohort]} cohort".center(80) if students[:cohort] == cohort }
-  puts
-  end
-end
-
-def print_footer(names)
-  if names.count == 0
-  end
-  if names.count == 1
-  puts "Overall, we have #{names.count} great student"
-else
-  puts "Overall, we have #{names.count} great students"
-end
 end
 
 def interactive_menu
-  students = []
   loop do
-  # 1. print the menu and ask the user what to do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit"
-  # 2. read the input and save it into a variable
-  selection = gets.chomp
-  # 3. do what the user has asked
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
   case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
+end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-  # 4. repeat from step 1
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
